a05fbbdde3f46977827512c0471923a47be214f6
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-16 15:34:37 +0800

    change test level_0 case.

--- test/level_0.c
+++ test/level_0.c
@@ -1,7 +1,112 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int Priority(char op)
+{
+    if (op=='#') {
+        return 0;
+    }
+    if (op=='+' || op=='-') {
+        return 1;
+    }
+    if (op=='*' || op=='/') {
+        return 2;
+    }
+    return -1;
+}
+
+double Operate(double x,double y,char op)
+{
+    if (op=='+') {
+        return x+y;
+    }
+    if (op=='-') {
+        return x-y;
+    }
+    if (op=='*') {
+        return x*y;
+    }
+    if (op=='/') {
+        return x/y;
+    }
+    return -1;
+}
+
+double Calc(char str[])
+{
+    double stDit[300];
+    char stOp[300];
+    int top1;
+    int top2;
+
+    double x;
+    double y;
+    double tmp;
+    char op;
+    int i;
+    int n = strlen(str);
+
+    top1 = -1;
+    top2 = -1;
+    stOp[++top2] = '#';
+    str[n++] = '#';
+
+    for(i=0; i < n; ++i)
+    {
+        if (str[i]==' ' || str[i] == '\n' || str[i] == '\t') {
+            continue;
+        }
+        if (isdigit(str[i]))
+        {
+            //数字
+            int n = 0;
+            while(isdigit(str[i])) {
+                n = n * 10 + str[i] - '0';
+                i++;
+            }
+            stDit[++top1] = n;
+            continue;
+        }
+        if(str[i] == '(') {            // (
+            stOp[++top2] = str[i];
+            continue;
+        }
+        if (str[i] == ')') {            // )
+            while(stOp[top2] != '(')  {
+                y = stDit[top1--];
+                x = stDit[top1--];
+                op = stOp[top2--];
+                stDit[++top1] = Operate(x,y,op);
+            }
+            top2 --;
+        }
+        else
+        {
+            while (Priority(stOp[top2]) >= Priority(str[i]))
+            {
+                //如果操作栈顶的操作符优先级高，则作+-*/运算
+                if (str[i]=='#' && stOp[top2]=='#') {
+                    return stDit[top1];
+                }
+                y = stDit[top1--];
+                x = stDit[top1--];
+                op = stOp[top2--];
+                stDit[++top1] = Operate(x,y,op);
+            }
+            //如果新操作符优先级高，str[i]进栈
+            stOp[++top2] = str[i];
+        }
+    }
+    return stDit[top1];
+}
+
 int main()
 {
-    // printf("Please enter an arithmetic expression, less than 100 characters:\n");
-    i = i + 1;
+    char str[100];
+    printf("Please enter an arithmetic expression, less than 100 characters:\n");
+    return 0;
 }
 
 
