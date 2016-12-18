587a9787453f43adf1e63ddc163a7264ebf1195e
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-18 09:10:24 +0800

    modify the level_1 test

--- test/level_1.c
+++ test/level_1.c
@@ -5,23 +5,25 @@ int main() {
     char str[100];
     printf("Please enter a string less than 100 character, terminated by an enter key:\n");
     gets(str);
+
     int i = 0;
-    int j = strlen(str) - 1;
+    int total = strlen(str) - 1;
     int flag = 1;
-    while (i < j) {
-        if (str[i] == str[j]) {
-            i = i + 1;
-            j = j - 1;
-        } else {
+
+    for (i = 0; i <= total; i++) {
+        if (str[i] != str[total - i]) {
+            // not a palindromic string
             flag = 0;
             break;
         }
     }
+
     if (flag == 1) {
         printf("True\n");
     } else {
         printf("False\n");
     }
+
     return 0;
 }
 
