c16f7df6b8b13ac39d76fde7190deab523038839
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-18 09:40:34 +0800

    modified the test case 2 to handle non-empty string.

--- test/level_2.c
+++ test/level_2.c
@@ -4,10 +4,29 @@
 int main(){
     char source[100];
     char target[100];
-    printf("Please enter a source string less than 100 characters:\n");
-    gets(source);
-    printf("Please enter a target string less than 100 characters:\n");
-    gets(target);
+
+    // get a non-empty source string
+    while (1)
+    {
+        printf("Please enter a non-empty source string less than 100 characters, terminated by an enter key:\n");
+        gets(source);
+        if (strcmp(source, ""))
+        {
+            break;
+        }
+    }
+
+    // get a non-empty target string
+    while (1)
+    {
+        printf("Please enter a target string less than 100 characters:\n");
+        gets(target);
+        if (strcmp(target, ""))
+        {
+            break;
+        }
+    }
+
     int source_len = strlen(source);
     int target_len = strlen(target);
 
@@ -15,6 +34,8 @@ int main(){
     int z = 0;
     int next_pose[100];
     next_pose[0] = -1;
+
+    /* calculate next_pos array for the target string */
     while (z < target_len)
     {
         if ((k == -1) || (target[z] == target[k]))
@@ -45,12 +66,19 @@ int main(){
                 j = next_pose[j];
             }
         }
+        /* matched the target string ! */
         if (j == target_len)
         {
-            if (flag == 0) {
+            if (flag == 0)
+            {
+                // matched for the first time
+                // not print comma
                 printf("%d", i - target_len);
                 flag = 1;
-            } else {
+            }
+            else
+            {
+                // otherwise print comma
                 printf(",%d", i - target_len);
             }
             j = 0;
@@ -60,8 +88,13 @@ int main(){
             break;
         }
     }
-    if (flag == 0) {
-        printf("False");
+    if (flag == 0)
+    {
+        printf("False\n");
+    }
+    else
+    {
+        printf("\n");
     }
     return 0;
 }
